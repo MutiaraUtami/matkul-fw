b097b7a5d431ab06146184c93f17d298343103fd
MutiaraUtami <utamimutiara20@gmail.com>
2025-09-03 15:35:52 +0700

    praktikum 2, route

--- praktikum/routes/web.php
+++ praktikum/routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about',function (){
+    return 'Ini Halaman About';
+})->name(name: 'about');
+
+Route::get('/contact',function (){
+    return ('Ini adalah halaman kontak');
+})->name(name: 'contact');
+
+Route::get('/users/{id}', function ($id) {
+    return "Nilai ID users adalah " . $id;
+});
+
+Route::prefix('manage')->group(function () {
+    Route::get('/edit', function () {
+        return "Ini adalah halaman manager/edit";
+    });
+    Route::get('/barang', function () {
+        return "Ini adalah halaman manager/barang";
+    });
+});
